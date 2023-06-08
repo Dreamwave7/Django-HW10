08dd7741ef384b9144dd4bd5fe70ba3f644ca6e5
Dreamwave7 <dima190179@gmail.com>
2023-06-08 22:43:34 +0300

    k

--- notes/noteapp/views.py
+++ notes/noteapp/views.py
@@ -1,8 +1,67 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import *
 
 # Create your views here.
 
 
 def main(request):
     return render(request, "noteapp/index.html")
+
+
+def tag(request):
+    if request.method == 'POST':
+        form = TagForm(request.POST)
+        if form.is_valid():
+            tag = form.save(commit=False)
+            tag.user = request.user
+            tag.save()
+            return redirect(to='noteapp:main')
+        else:
+            return render(request, 'noteapp/tag.html', {'form': form})
+
+    return render(request, 'noteapp/tag.html', {'form': TagForm()})
+
+
+
+def note(request):
+    tags = Tag.objects.filter(user=request.user).all()
+
+    if request.method == 'POST':
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            new_note = form.save(commit=False)
+            new_note.user = request.user
+            new_note.save()
+            choice_tags = Tag.objects.filter(name__in=request.POST.getlist('tags'), user=request.user)
+            for tag in choice_tags.iterator():
+                new_note.tags.add(tag)
+
+            return redirect(to='noteapp:main')
+        else:
+            return render(request, 'noteapp/note.html', {"tags": tags, 'form': form})
+
+    return render(request, 'noteapp/note.html', {"tags": tags, 'form': NoteForm()})
+
+
+def detail(request, note_id):
+    note = get_object_or_404(Note, pk=note_id)
+    return render(request, 'noteapp/detail.html', {"note": note})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
